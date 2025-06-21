3aa645412c7097d9fad77cd170e98ea0b3396b51
ffanliu <2592965531@qq.com>
2025-06-21 21:52:32 +0800

    前后端结合第十四节课

--- src/views/new/login.vue
+++ src/views/new/login.vue
@@ -1,48 +1,29 @@
 <script setup>
 import {ref} from "vue";
-//引入axios
 import axios from "axios";
-//发请求
-//处理请求
-const loginName = ref('')
-const password = ref('')
-const login = () => {
-  //axios是异步请求
-  //get(url, {params: {}})
-  //url?xxx=yy&aa=bb
-  //字符串拼接
-  axios.get('/api/suibian?loginName='+loginName.value+'&password='+password.value)
-      .then((res)=>{
-    //请求发送成功并且响应成功要做的事
-    console.info(11111)
-    console.info(res)
-  }).catch((error)=>{
-    console.info(error)
-  }).finally(()=>{
 
-  })
-  console.info(22222)
-  //axios提供的api
-  axios.get('/api/suibian', {params: {loginName: loginName.value, password: password.value}})
-  //占位符
-  axios.get(`/api/suibian?loginName=${loginName.value}&password=${password.value}`)
-
-  //post(url, data) 发送的是json数据
-  axios.post('/api/suibianpost', {loginName: loginName.value, password: password.value}).then((res)=>{
-    //请求发送成功并且响应成功要做的事
-    console.info(res)
-  }).catch((error)=>{
-    console.info(error)
-  }).finally(()=>{
-
+const user = ref({
+  a: '',
+  b: ''
+})
+const login = ()=>{
+  const u = {
+    loginName: user.value.a,
+    password: user.value.b
+  }
+  axios.post('/api/user/login', u).then(res => {
+    //res是axios封装后的运行结果
+    //res.data才是后端返回的json
+    //res.data.data才是数据库查询出来的数据
+    console.info(res.data.data)
   })
 }
 </script>
 
 <template>
 <div>
-  用户名 <input type="text" v-model="loginName"> <br>
-  密码 <input type="password" v-model="password"> <br>
+  用户名<input type="text" v-model="user.a"> <br>
+  密码<input type="text" v-model="user.b"> <br>
   <button @click="login">登录</button>
 </div>
 </template>
